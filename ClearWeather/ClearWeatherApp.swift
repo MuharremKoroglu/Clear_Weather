da0ea7b2fd7d0713208f8a929929d32c35d06cc7
Muharrem Koroglu <muharrem-koroglu.99@hotmail.com>
2023-03-17 16:36:56 +0300

    Initial Commit

---
+++ ClearWeather/ClearWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearWeatherApp.swift
+//  ClearWeather
+//
+//  Created by Muharrem Köroğlu on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ClearWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
